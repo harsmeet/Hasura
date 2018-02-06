77933020ec1d228b782f6241df5a98aec5685521
harsmeet <er.harsmeet@gmail.com>
2018-02-06 16:19:34 +0530

    some normal commit

---
+++ src/main/java/com/example/hars/hasura/MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.hars.hasura;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+TextView textView;
+EditText editText;
+Button button, intentB;
+    private static final String TAG="MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+         textView=(TextView)findViewById(R.id.textView2);
+//        setting the text for the text view
+//        textView.setText("hello_World");
+        editText=(EditText)findViewById(R.id.editText2);
+        button=(Button)findViewById(R.id.button2);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Toast.makeText(MainActivity.this,"Button Clicked",Toast.LENGTH_SHORT).show();
+
+                String enteredText=editText.getText().toString();
+                if (enteredText.isEmpty()) {
+//                    show alert
+                    showAlert();
+                }else{
+//replace text in textView
+                    replaceText(enteredText);
+                }
+            }
+        });
+
+        Log.i(TAG,"Inside On Create");
+    }
+//    lets make the method to replaceText
+    private void replaceText(String newText){
+        textView.setText(newText);
+
+        editText.setText("");
+    }
+private void showAlert(){
+//    Building the Dialog
+    AlertDialog.Builder builder=new AlertDialog.Builder(this);
+    builder.setTitle("Khalli");
+    builder.setMessage("No string to replace");
+//    also access neutra button
+    builder.setNeutralButton("Cancel", new DialogInterface.OnClickListener() {
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
+            dialog.dismiss();
+        }
+    });
+//    show the dilaog
+    builder.show();
+}
+
+
+}
